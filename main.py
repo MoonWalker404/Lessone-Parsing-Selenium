8b57c3ad79aeb6a893c4371c69c325b2d8cf2209
MoonWalker404 <crossbeat71@gmail.com>
2024-07-02 17:04:41 +0300

    add homework selenium

--- main.py
+++ main.py
@@ -4,18 +4,44 @@ from selenium.webdriver.common.by import By
 import random
 import time
 
-browser = webdriver.Chrome()
-browser.get("https://ru.wikipedia.org/wiki/%D0%A1%D0%BE%D0%BB%D0%BD%D0%B5%D1%87%D0%BD%D0%B0%D1%8F_%D1%81%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D0%B0")
-time.sleep(20)
 
-hatnotes = []
+def nw():
+    browser = webdriver.Firefox()
+    browser.get("https://ru.wikipedia.org")
 
-for element in browser.find_elements(By.TAG_NAME, "div"):
-    cl = element.get_attribute("class")
-    if cl == "hatnote navigation-not-searchable":
-        hatnotes.append(element)
+    user_input = input("Введите запрос: ")
+    search_box = browser.find_element(By.ID, 'searchInput')
+    search_box.send_keys(user_input)
+    search_box.send_keys(Keys.RETURN)
+    time.sleep(3)
 
-print(hatnotes)
-hatnote = random.choice(hatnotes)
-link = hatnote.find_element(By.TAG_NAME, "a").get_attribute("href")
-browser.get(link)
+    while True:
+        print("\nТекущая статья", browser.title)
+        paragraphs = browser.find_elements(By.CLASS_NAME, 'mw-search-result-heading')
+        links = browser.find_elements(By.TAG_NAME, 'a')
+        print("Выберите действия: ")
+        print("1 - Листать параграфы")
+        print("2 - Перейти на одну из связанных страниц")
+        print("3 - Выйти из программы")
+        choice = input("Ваш выбор: ")
+        if choice == "1":
+            for index, paragraph in enumerate(paragraphs):
+                print(f"Параграф {index + 1}:{paragraph.text}")
+                if input("Нажмите Q для выхода") == "Q":
+                    break
+        elif choice == "2":
+            for index, link in enumerate(links):
+                print(f"{index + 1}:{link.get_attribute('href')}-{link.text}")
+            link_choice = int(input(f"Выберите страницу для перехода")) - 1
+            links[link_choice].click()
+            time.sleep(3)
+        elif choice == "3":
+            break
+        else:
+            print(f"Неизвестная команда, попробуйте снова")
+    browser.quit()
+
+
+
+if __name__ == '__main__':
+    nw()
